d614ba9ba2d86c5ac9953acf6f30ded11e216850
Grace Gamble <grace.h.gamble@gmail.com>
2019-02-27 11:55:58 -0700

    Convert App Component to Class Based Component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-      window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-      );
-      return (
-            <div>
-                  Latitude: 
-                  <SeasonDisplay />
-            </div>
-      );
-};
+class App extends Component {
+      render() {
+            window.navigator.geolocation.getCurrentPosition(
+                  (position) => console.log(position),
+                  (err) => console.log(err)
+            );
+            return (
+                  <div>
+                        Latitude:
+                        <SeasonDisplay />
+                  </div>
+            );
+      }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
