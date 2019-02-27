cf0f8485c6c99f2aaf679521e22abbbd7df111d2
Grace Gamble <grace.h.gamble@gmail.com>
2019-02-27 12:25:20 -0700

    State with Constructor and Set State with Latitude

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends Component {
-      render() {
+      constructor(props) {
+            super(props);
+            this.state = {
+                  lat: null
+            };
             window.navigator.geolocation.getCurrentPosition(
-                  (position) => console.log(position),
+                  (position) => {this.setState({lat: position.coords.latitude})},
                   (err) => console.log(err)
             );
+      }
+      render() {
             return (
                   <div>
-                        Latitude:
-                        <SeasonDisplay />
+                        Latitude: {this.state.lat}
                   </div>
             );
       }
